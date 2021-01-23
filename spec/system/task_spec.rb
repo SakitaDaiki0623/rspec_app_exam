41e9105e1d1d509ec6f7c7368e49b9bcd2c2f1f6
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-01-23 17:36:59 +0900

    TaskのFactoryにassociationを定義

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Task', type: :system do
   let(:project) { create(:project) }
-  let(:task) { create(:task, project_id: project.id) }
+  let(:task) { create(:task) }
   describe 'Task一覧' do
     context '正常系' do
       it '一覧ページにアクセスした場合、Taskが表示されること' do
@@ -77,7 +77,7 @@ RSpec.describe 'Task', type: :system do
 
       it '既にステータスが完了のタスクのステータスを変更した場合、Taskの完了日が更新されないこと' do
         # TODO: FactoryBotのtraitを利用してください
-        done_task = create(:task, :done_task, project_id: project.id)
+        done_task = create(:task, :done_task)
         visit edit_project_task_path(project, done_task)
         select 'todo', from: 'Status'
         click_button 'Update Task'
@@ -91,7 +91,7 @@ RSpec.describe 'Task', type: :system do
   describe 'Task削除' do
     context '正常系' do
       # FIXME: テストが失敗するので修正してください
-      let!(:task) { create(:task, project_id: project.id) }
+      let!(:task) { create(:task) }
       it 'Taskが削除されること' do
         visit project_tasks_path(project)
         click_link 'Destroy'
